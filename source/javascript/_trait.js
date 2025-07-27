f3d0bfe42b5692cd3161b1409792150f3ac402dc
James Ojaste <james.ojaste@gmail.com>
2025-07-27 17:08:15 -0400

    trait details to chat working

--- source/javascript/_trait.js
+++ source/javascript/_trait.js
@@ -1,11 +1,26 @@
-// post the trait to chat using a template
-const postTrait = (trigger,attributes,sections,casc) => {
+/**
+ * Post a trait to chat using a template
+ * @param {object} trigger - The trigger that caused the function to be called
+ * @param {object} attributes - The attribute values of the character
+ * @param {object[]} sections - All the repeating section IDs
+ * @param {object} casc - Expanded cascade object
+ */
+const postTrait = async function({trigger,attributes,sections,casc}) {
 	console.log ('postTrait()', trigger, attributes, sections, casc);
-}
-K.registerFuncs({toggleTrait});
 
-// collapse/expand the trait section
-const toggleTrait = (trigger,attributes,sections,casc) => {
-	console.log ('toggleTrait()', trigger, attributes, sections, casc);
+	const rollName = trigger.name.replace(/-action$/,'');
+	const rollSource = rollName.split('-')[0];
+	const rollAttr = rollName.replace(/-/g,'_');
+	const rollTransKey = rollName.replace(/-/g,' ');
+	const rollObj = {
+		title: attributes[rollAttr],
+		source: rollSource,
+		requirements: attributes[`${rollAttr}_requirements`],
+		description: attributes[`${rollAttr}_text`]
+		};
+	console.log ("executeRoll()", rollName, rollAttr, rollTransKey, rollObj);
+	const roll = await executeRoll({rollObj,attributes,sections});
+
+	finishRoll(roll.rollId);
 }
-k.registerFuncs({toggleTrait});
+k.registerFuncs({postTrait});
